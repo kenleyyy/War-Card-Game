9bc9624c6cb62c6ed489a13a0af848ddd2a35d92
kenley <108115991+kenleyyy@users.noreply.github.com>
2024-05-07 17:10:40 -0400

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Kenley Hilaire on 5/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var playerCard = "card"
+    @State var cpuCard = "card"
+    
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Image("background-plain").resizable().ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                    
+                }
+                Spacer()
+                            
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+                
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(
+                            .bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline).padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
+         
+            
+        }  } 
+    
+    func deal() {
+        // Randomizes the player's card
+
+        let playerCardValue = Int.random(in: 2...14)
+        
+        playerCard = "card" + String(playerCardValue)
+        
+        // Randomizes the cpu's card
+        let cpuCardValue = Int.random(in: 2...14)
+        
+        cpuCard = "card" + String(cpuCardValue)
+        
+        // Update the score
+        if playerCardValue > cpuCardValue {
+            playerScore += 1
+        }
+        else if cpuCardValue > playerCardValue {
+            cpuScore += 1
+        }
+
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
